725e5fdbcc3e73de3fbfd9f346bc2649f63e27c3
Sarup Banskota <sbanskota08@gmail.com>
2013-12-31 22:29:20 +0530

    Update with the openshift stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Dinadarshica::Application.configure do
+RailsApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
