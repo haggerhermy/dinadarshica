453107f6d5244fed240bdc45858655e7e44a0d9a
Sarup Banskota <sbanskota08@gmail.com>
2014-01-01 00:36:16 +0530

    Add routes etc for users - phew

---
+++ db/migrate/20131231181110_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :bio
+      t.string :interests
+      t.integer :followers
+      t.integer :follows
+
+      t.timestamps
+    end
+  end
+end
